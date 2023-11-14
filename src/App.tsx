6ad06326a2f775401ffbb7d01ba3ec586c513b0d
svanlaugbirna <svanlaugbirna@gmail.com>
2023-11-14 09:28:23 +0000

    setja inn button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [greeting, setGreeting] = useState ({greeting:"Hello", counter: 0});
+  const changeGreeting = () => {
+setGreeting ({ greeting: "good bye", counter: 1})
+  }
+  console.log(greeting);
+return (
+<div>
+  <button onClick={changeGreeting}>Click me</button>
+  {greeting.greeting}
+</div>
+
+);
+
 }
 
 export default App;
+
---
+++ src/Button.tsx
@@ -0,0 +1,18 @@
+const Button = ()=>{
+    const styles ={ 
+        backgroundColor: "blue",
+        borderRadius: "5px",
+        height: "40px",
+        width: "200px",
+        color: "white"
+    }
+
+return (
+    <> 
+    <button style={styles}>Klikk</button>
+    </>
+)
+
+} 
+
+export default Button;
